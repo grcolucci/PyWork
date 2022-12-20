560e5a6562d14cc305df04cc32e87cdd70c046b8
grcolucci <glenn.colucci@gmail.com>
2022-12-19 23:32:49 -0500

    New

--- slider.py
+++ slider.py
@@ -1,12 +1,13 @@
 from tkinter import *
+import matplotlib
 
 def show_values():
     print (w1.get(), w2.get())
 
 master = Tk()
-w1 = Scale(master, from_=0, to=42)
+w1 = Scale(master, from_=0, to=42, length=400, tickinterval=2, orient=HORIZONTAL)
 w1.pack()
-w2 = Scale(master, from_=0, to=200, orient=HORIZONTAL)
+w2 = Scale(master, from_=0, to=200, length=400, tickinterval=10, orient=HORIZONTAL)
 w2.pack()
 Button(master, text='Show', command=show_values).pack()
 
