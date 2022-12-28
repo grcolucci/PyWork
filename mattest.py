a9f0e35603c1bf4084e03154ba5c92a3a0329f73
grcolucci <114946658+grcolucci@users.noreply.github.com>
2022-12-27 22:05:06 -0500

    new

--- mattest.py
+++ mattest.py
@@ -1,12 +1,53 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+import math
+from tkinter import *
+import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
-x = np.arange(0, 4, 0.05)
-y = np.sin(x*np.pi)
+signalArray = [0.0] * 1000
+timeArray = [0.0] * 1000
+ampSlider = 0
+freqSlider = 0
+def show_values():
+    timeCnt = 0.0
+    ampInd = ampSlider.get()
+    freqInd = freqSlider.get()
+    for x in range(len(signalArray)):
+        timeArray[x] = timeCnt
+        signalArray[x] = ampInd * math.cos((2*math.pi) * freqInd * timeCnt)
+        timeCnt += 0.001
 
-fig, ax = plt.subplots(figsize=(3,2), constrained_layout=True)
-ax.plot(x, y)
-ax.set_xlabel('t [s]')
-ax.set_ylabel('S [V]')
-ax.set_title('Sine wave')
-fig.set_facecolor('lightsteelblue')
+
+    x = timeArray ## np.arange(0, 1.1, 0.2)
+    y = signalArray ##np.sin(x*np.pi)
+
+    fig, ax = plt.subplots(figsize=(3,2), constrained_layout=True)
+    ax.plot(x, y)
+    ax.set_xlabel('Frequency')
+    ax.set_ylabel('Amplitude')
+    ax.set_title('Sine wave')
+    fig.set_facecolor('lightsteelblue')
+    fig.set_size_inches (12,8)
+    fig.set
+
+    ## plt.show()
+    canvas = FigureCanvasTkAgg(fig,
+                               master = window)  
+    canvas.draw()
+
+
+master = Tk()
+#Scale(frame, from_=0.55, to=4.75, digits = 3, resolution = 0.01, label = "Lower")
+ampSlider = Scale(master, from_=0.00, to=10.2, digits=3, resolution = 0.01, label="Amplitude", length=800, tickinterval=0.02, orient=HORIZONTAL)
+ampSlider.set(3.6)
+ampSlider.pack()
+freqSlider = Scale(master, from_=0, to=250, length=800, tickinterval=10, label="Frequency", orient=HORIZONTAL)
+freqSlider.set(50)
+freqSlider.pack()
+Button(master, text='Show', command=show_values).pack()
+
+mainloop()
+
+
+
--- sinsig.py
+++ sinsig.py
@@ -3,12 +3,10 @@ import math
 import numpy as np
 from plotter import Plotter
 
-
 signalArray = [0] * 1000
 timeArray = [0] * 1000
 ampSlider = 0
 freqSlider = 0
-
 
 def show_values():
 
@@ -19,7 +17,6 @@ def show_values():
                 timeArray[x] = timeCnt
                 signalArray[x] = ampInd * math.cos((2*math.pi) * freqInd * timeCnt)
                 timeCnt += 0.001        
-        # print (signalArray)
 
         #x = np.linspace(-np.pi, np.pi, 201)
 
@@ -33,9 +30,12 @@ def show_values():
 
 
 
-master = Tk()
+master = Tk() 
+master.title('Mytitle')
+master.geometry("740x840")
+
 #Scale(frame, from_=0.55, to=4.75, digits = 3, resolution = 0.01, label = "Lower")
-ampSlider = Scale(master, from_=0.00, to=10.2, digits=3, resolution = 0.01, label="Amplitude", length=800, tickinterval=0.02, orient=HORIZONTAL)
+ampSlider = Scale(master, from_=0.00, to=10.2, digits=3, resolution = 0.02, label="Amplitude", length=1200, tickinterval=0.02, orient=HORIZONTAL)
 ampSlider.set(3.6)
 ampSlider.pack()
 freqSlider = Scale(master, from_=0, to=250, length=800, tickinterval=10, label="Frequency", orient=HORIZONTAL)
